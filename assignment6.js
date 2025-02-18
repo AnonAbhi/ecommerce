27364c7c2566c8b51522ffc550de9282b6329478
AnonAbhi <160837328+AnonAbhi@users.noreply.github.com>
2025-02-18 10:03:08 +0530

    added index.html file

---
+++ assignment6.js
@@ -0,0 +1,6 @@
+function UpdateCart(){
+            let cart=document.getElementById("cart");
+            cart.innerHTML=parseInt(cart.innerHTML)+1;
+            cart.classList+="show";
+
+        }
